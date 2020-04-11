6d9e62e8821e4cd4f6bc8543ce39a7ec4e0984ae
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2020-04-11 11:20:33 +0530

    Update attempt1.py

--- Rupsa and the Game (RGAME)/attempt1.py
+++ Rupsa and the Game (RGAME)/attempt1.py
@@ -1,1 +1,23 @@
+T = input()
 
+T = int(T)
+total_sum = []
+
+for i in range(0,T):
+  N = input()
+  N = int(N)
+  
+  sum = 0
+  numbers = input()
+  sequence = numbers.split()
+  
+  sequence = [int(i) for i in sequence] 
+  
+  for j in range(0,len(sequence)):
+    for k in range(j+1,len(sequence)):
+      sum = sum + (sequence[j]*sequence[k]*(2**(N+1-k)))
+      
+  total_sum.append(sum)
+  
+for i in range(0,len(sequence)):
+  print(total_sum[i])
