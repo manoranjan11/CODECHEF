623e46edc50311e1b472d1d7211503786f309570
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2020-04-16 22:53:43 +0530

    Update titled_1.py

--- Sell All the Cars (CARSELL)/titled_1.py
+++ Sell All the Cars (CARSELL)/titled_1.py
@@ -3,7 +3,14 @@ T = int(raw_input())
 for _ in range(0,T):
   N = int(raw_input())
   
-  for i in range(0,N):
-    P = raw_input().split(" ")
-    P = [long(i) for i in P] 
+  P = list(map(long,raw_input().split()))
     
+  profit = 0
+  
+  P.sort(reverse = True)
+  
+  for i in range(0,N):
+    if P[i] - i < 0:
+      profit = profit + P[i] -i
+      
+  print profit   
