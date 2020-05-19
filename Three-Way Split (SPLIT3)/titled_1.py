fda0b706381c778f79f4bd175003184dabe1a1a1
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2020-05-19 23:11:03 +0530

    Update titled_1.py

--- Three-Way Split (SPLIT3)/titled_1.py
+++ Three-Way Split (SPLIT3)/titled_1.py
@@ -1,1 +1,15 @@
+N = int(input())
 
+traffic = []
+way = {}
+for i in range(0,N):
+  temp = int(input())
+  traffic.append(temp)
+  way[i+1] = []
+
+for i in range(0,N-1):
+  x,y = map(int,input().split())
+  
+  way[x] = way[x].append(y)
+  way[y] = way[y].append(x)
+    
