100df4447a7af5e00d426e94aab8c9301e307784
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2020-05-19 14:58:22 +0530

    Update titled_1.py

--- COVID Pandemic and Long Queue (COVIDLQ)/titled_1.py
+++ COVID Pandemic and Long Queue (COVIDLQ)/titled_1.py
@@ -1,1 +1,25 @@
+# cook your dish here
+# perfect code
+T = int(input())
 
+for i in range(0,T):
+  N = int(input())
+  
+  A = list(map(int,input().split()))
+  
+  temp = 10 #temporary variable
+  status = 0 #status of the text case
+  for i in range(0,len(A)):
+    if A[i] == 0 & temp == 10: #checking the first person in the queue
+      continue
+    elif A[i]==0:
+      temp = temp+1
+    else:
+      if temp<5:  
+        print("NO")
+        status = 1
+        break
+      temp = 0
+      
+  if status==0:
+    print("YES")
