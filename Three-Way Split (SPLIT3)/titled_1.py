c8d43de1d124744d7c9ae5a39e62aa5957ba3dc9
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2020-05-20 11:54:43 +0530

    Update titled_1.py

--- Three-Way Split (SPLIT3)/titled_1.py
+++ Three-Way Split (SPLIT3)/titled_1.py
@@ -20,13 +20,9 @@ cost3 = 0
 nodes_crossed = []
 possibilities = []
 
-def travel(start_node,branches,nodes_passed):
-  cost = 0
-  
-  nodes_passed = nodes_passed.append(start_node)
-  for node in way[start_node]:
-    branches = branches + len(way[start_node])
+def travel(start_node,end_node):
+ 
   
 for i in range(0,N-1):
-  if len(way[i+1]) == 1:
-    travel(i+1)
+    for j in range(i,N):
+      travel(i,j)
