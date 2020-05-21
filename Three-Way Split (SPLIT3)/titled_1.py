90b6c782de1eda5347f0fd6aaa9d515f06e7e696
manoranjan11 <39557252+manoranjan11@users.noreply.github.com>
2020-05-21 18:52:23 +0530

    Update titled_1.py

--- Three-Way Split (SPLIT3)/titled_1.py
+++ Three-Way Split (SPLIT3)/titled_1.py
@@ -19,49 +19,71 @@ discontinuity = 0
 discontinuity_nodes = []
 possibilities = []
 path = []
-
+previous_node = 0
 
 def travel(start_node,end_node):
   global discontinuity
-  print("I m in travel mode")
+  global previous_node
   path.append(start_node)
-  print(path)
+   
   for node in way[start_node]:
     if node == end_node:
       path.append(node)
+      size1 = len(way[node])
+      if size1 != 1:
+        discontinuity += size1-1
+        discontinuity_nodes.append(node)
       return 1
-  
   length = len(path)
+  if length>=2:
+    previous_node = path[length -2]
+
   for node in way[start_node]: 
     size = len(way[node])
-    if size == 1 | node == path[length - 1]:
+    
+    if size == 1 or node == previous_node:
       continue
     else:
+      
       discontinuity += size-2
-      discontinuity_nodes.append(start_node)
+      discontinuity_nodes.append(node)
+            
       if discontinuity>2:
         continue
       temp = travel(node,end_node)
-      return 1
-  discontinuity += -size+2
-  path.remove(node)
-  discontinuity_nodes.remove(node)
+      if temp == 0:
+        continue
+      else:
+        return 1
+  discontinuity += -len(way[start_node])+2
+  path.remove(start_node)
+  discontinuity_nodes.remove(start_node)
   return 0    
 
 def calculate(node):
+  global cost
+  #print(cost)
+  cost = cost + traffic[node]
   for n in way[node]:
     if n not in path:
       cost = cost + traffic[n]
       if len(way[n]) !=1:
         calculate(n)
              
-for i in range(1,2):
-    for j in range(2,3):
+for i in range(1,N):
+    for j in range(i,N+1):
       temp = travel(i,j)
+      if len(way[i])>1:
+         discontinuity += len(way[i])-1
+         discontinuity_nodes.append(i)
+      #print(path)
+      #print(discontinuity)
+      #print("discontinuity nodes:",discontinuity_nodes)
+      previous_node = 0
       if discontinuity == 2:
         for k in path:
           cost = cost + traffic[k]
-        discontinuity = 0
+          #print(cost)
        
         option = []
         option.append(cost)
@@ -69,8 +91,22 @@ for i in range(1,2):
         
         for k in discontinuity_nodes:
           for node in way[k]:
+            #print(node)
             if node not in path:
+              #print(node)
               calculate(node)
               option.append(cost)
               cost = 0
-      	possibilities = possibilities.append(option)
+        possibilities.append(option)
+        
+      del path[0:len(path)]
+      del discontinuity_nodes[0:len(discontinuity_nodes)]
+      discontinuity = 0
+#print(possibilities)
+
+maxi = max(possibilities[0])
+for chance in possibilities:
+  if maxi > max(chance):
+    maxi = max(chance)
+
+print(maxi)
